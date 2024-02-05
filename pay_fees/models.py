778b6efbbc41ad9be5959531795a178791640277
phillip saint <phillipsantei254@gmail.com>
2024-02-05 16:13:13 +0300

    correct display of transaction information

--- pay_fees/models.py
+++ pay_fees/models.py
@@ -214,7 +214,7 @@ class Transaction(models.Model):
     response_description = models.CharField(max_length=250, null=True)
 
     def __str__(self):
-        return f"<{self.transaction_id}> {self.first_name} {self.last_name}"
+        return f"<{self.id}> {self.student.user.first_name} {self.student.user.last_name}"
 
 
 class PaymentMethods(models.Model):
